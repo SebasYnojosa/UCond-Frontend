30d6c479af79906b8d9b8c22991c42183858cbda
wincancode <erodriguez.win@gmail.com>
2023-11-11 17:46:27 -0400

    fafafa

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import './App.css'
 import { Banner } from './components/Banner';
 import { Pricing } from './components/Pricing';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from './components/button';
 
 function App() {
-
   return (
     <div className="UCond">
       <NavBar />
       <Banner />
       <Pricing />
+      <Button />
     </div>
-    
-  )
+  );
+  
 }
 
 export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+
+
+export const Button = () => {
+    return (
+        <div>
+            <h1>button</h1>
+        </div>
+    )
+}
